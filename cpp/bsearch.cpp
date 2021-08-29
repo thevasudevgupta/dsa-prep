85226613f6a94138cef674af961265ca4762418a
Vasudev <7vasudevgupta@gmail.com>
2021-08-29 05:56:09 +0530

    better view

--- cpp/bsearch.cpp
+++ cpp/bsearch.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+// Binary search (array needs to be sorted)
 
+// O(log(n)) ; O(1)
 int bsearch(int arr[], int val, int low, int high) {
     int mid = (low + high) / 2;
-    if (low == high & arr[mid] != val) {
+    if (low == high && arr[mid] != val) {
         return -1;
     }
     if (arr[mid] == val) {return mid;}
-    if (arr[mid] > val) {
+    else if (arr[mid] > val) {
         high = mid - 1;
         return bsearch(arr, val, low, high);
     }
@@ -21,5 +23,5 @@ int main() {
     int val = 53;
     int low = 0, high = sizeof(arr) / sizeof(*arr);
 
-    std::cout << bsearch(arr, val, low, high);
+    std::cout << bsearch(arr, val, low, high) << std::endl;
 }
